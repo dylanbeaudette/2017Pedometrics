2560013e1a52fb01f22392e94877dec80804b1d4
Dylan Beaudette <dylan.beaudette@gmail.com>
2017-06-16 11:11:42 -0700

    removed some junk components (this is still an active survey) and added connotative legend

--- Network-talk/CA630/generate-network.R
+++ Network-talk/CA630/generate-network.R
@@ -13,19 +13,9 @@ load('cached-nasis.rda')
 # convert component data into adjacency matrix, weighted by component percentage
 m <- component.adj.matrix(x, mu='musym', co='compname', wt='comppct_r')
 
+## TODO: hand-make graph for more control
 # make graph using defaults specified in sharpshootR::plotSoilRelationGraph()
-# TODO: hand-make graph for more control
-
-# save a copy of the output for expert review
-pdf(file='ca630-network.pdf', width=15, height=15)
-par(mar=c(0,0,2,0))
-g <- plotSoilRelationGraph(m, vertex.scaling.factor=1, main='Calaveras/Tuolumne Co. Soil Survey', vertex.label.family='sans', vertex.label.cex=0.65)
-# nasty hack to get a reasonable legend
-leg <- unique(data.frame(cluster=V(g)$cluster, color=V(g)$color, stringsAsFactors = FALSE))
-leg <- leg[order(leg$cluster), ]
-legend('bottomleft', legend=leg$cluster, col=leg$color, pch=15, ncol = 4)
-dev.off()
-
+g <- plotSoilRelationGraph(m, main='Calaveras/Tuolumne Co. Soil Survey', plot.style = 'none')
 
 # community / cluster labels are returned as of
 # https://github.com/ncss-tech/sharpshootR/commit/5797803c46b1043f658d852624d09ca15df89f17
@@ -34,23 +24,37 @@ V(g)$cluster
 ## important note: the graph and associated communities / colors are stable between runs (set.seed used in plotSoilRelationGraph)
 ## this means we can rely on the cluster numbers as an index to expert interpretation
 
+# load expert interp and add to graph attributes
+d.interp <- read.csv(file='expert-interp.csv', stringsAsFactors = FALSE)
+V(g)$notes <- d.interp$notes[match(V(g)$cluster, d.interp$cluster)]
+
 # extract vertex attributes for interpretation and linking to MU data
 d <- as_data_frame(g, what='vertices')
 names(d)[1] <- 'compname'
 
 
-# make text file with component names within each cluster
-sink(file = 'cluster-correlation-notes.txt')
-d_ply(d, 'cluster', function(i) {
-  cat(paste0('--------------', i$cluster[1], '---------------\n'))
-  write.table(i[, c('compname')], row.names=FALSE, col.names = FALSE)
-  })
-sink()
+## only need to do this to help with expert interpretation
+# # make text file with component names within each cluster
+# sink(file = 'cluster-correlation-notes.txt')
+# d_ply(d, 'cluster', function(i) {
+#   cat(paste0('--------------', i$cluster[1], '---------------\n'))
+#   write.table(i[, c('compname')], row.names=FALSE, col.names = FALSE)
+#   })
+# sink()
 
 
-# join-in expert interpretation
-d.interp <- read.csv(file='expert-interp.csv', stringsAsFactors = FALSE)
-d <- join(d, d.interp, by='cluster')
+# nasty hack to get a reasonable legend
+leg <- unique(data.frame(cluster=V(g)$cluster, color=V(g)$color, notes=V(g)$notes, stringsAsFactors = FALSE))
+leg <- leg[order(leg$cluster), ]
+
+# save a copy of the output for expert review
+pdf(file='ca630-network.pdf', width=15, height=15)
+par(mar=c(0,0,2,0))
+plotSoilRelationGraph(m, vertex.scaling.factor=1, main='Calaveras/Tuolumne Co. Soil Survey', vertex.label.family='sans', vertex.label.cex=0.65)
+
+legend('bottomleft', legend=paste0(leg$cluster, ') ', leg$notes), col=leg$color, pch=15, ncol = 4, cex=0.5)
+dev.off()
+
 
 
 # save
